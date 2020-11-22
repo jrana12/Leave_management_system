d8da91fa1cb6fcde72291c1a0f2b9caef1383cfb
jrana12 <62606186+jrana12@users.noreply.github.com>
2020-11-22 11:04:52 +0530

    Update keys.js

--- server/keys.js
+++ server/keys.js
@@ -1,4 +1,4 @@
 module.exports ={
-	MONGOURI:"mongodb+srv://Jaimini_Instagram:HoT6HB6pvKZGKwPA@cluster0.sp0j2.mongodb.net/DB1?retryWrites=true&w=majority",
+	MONGOURI:"****KEY HIDDEN****",
 	JWT_SECRET:"jaimini"
 }
